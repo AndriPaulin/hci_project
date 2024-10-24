72834570c3238658fa4796d8c95f553f28db6307
Andri Paulin <andripaulin@MacBook-Pro-von-Andri.local>
2024-10-24 20:45:32 +0200

    added documentation to task.component.ts

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -41,33 +41,48 @@ export class TaskComponent {
 
   /**
    * Sets editMode to false if user is finished editing a task.
-   * This only works locally and does not notify task-service of the change; This functionality might need to be added.
+   * Calls this.safeToTaskService().
    */
   confirmChanges():void {
     this.task.editMode = false;
-
-
-
     this.safeToTaskService(); //this would make more sense if we were to safe to a Database. Right now we're just saving to the array in taskService and then loading from it again... Kind of pointless, but makes some sense from a conceptual point of view.
   }
 
   /**
-   *
+   * Is called if a task is to be deleted.
+   * Emits an Event to board.component.
+   * The deletion is further handled by the board.
    */
   deleteTask(): void {
     this.deleteTaskEvent.emit(this.task);
   }
 
+  /**
+   * Adds a new subtask by pushing it to the subTasks-array.
+   * The new subtask is outfitted with default values.
+   */
   addNewSubtask(): void {
     this.task.subTasks.push(new SubTaskModel("neue Aufgabe", false))
   }
 
+  /**
+   * Safes the task to taskService.
+   * Is called if a change is registered in one of the inputs of the task or its subtasks.
+   * Is called by this.confirmChanges().
+   * Is called if a subtask registers a change on its checkbox.
+   *
+   * Calls this.checkIfCompleted() and this.setBGColor.
+   */
   safeToTaskService(): void {
     this.task.checkIfCompleted(); //method of task.model.ts. It might be better to move the check to task.component.ts
     this.setBGColor();
     this.updateTaskEvent.emit(this.task) //Reminder: check if subtasks are updated!
   }
 
+  /**
+   * Decides which class to use for background color depending on whether the task is completed or not.
+   * Uses one-way-binding to insert the correct class name in task.component.html.
+   */
   setBGColor():void {
     if (this.task.taskCompleted){
       this.BGColor = "bg-green";
